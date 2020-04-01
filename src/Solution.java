c10611c788fbd75c6da8cd30358ec3a5be875817
jaimea <jaime.hernan95@gmail.com>
2020-04-01 16:14:07 -0400

    images

--- src/Solution.java
+++ src/Solution.java
@@ -1,4 +1,4 @@
-import java.util.Scanner;
+import java.util.*;
 
 class Person {
     protected String firstName;
@@ -6,33 +6,52 @@ class Person {
     protected int idNumber;
 
     // Constructor
-    Person(String firstName, String lastName, int identification) {
+    Person(String firstName, String lastName, int identification){
         this.firstName = firstName;
         this.lastName = lastName;
         this.idNumber = identification;
     }
 
     // Print person data
-    public void printPerson() {
-        System.out.println("Name: " + lastName + ", " + firstName + "\nID: " + idNumber);
+    public void printPerson(){
+        System.out.println(
+                "Name: " + lastName + ", " + firstName
+                        + 	"\nID: " + idNumber);
     }
+
 }
 
-class Student extends Person {
+class Student extends Person{
     private int[] testScores;
+
+    /*
+     *   Class Constructor
+     *
+     *   @param firstName - A string denoting the Person's first name.
+     *   @param lastName - A string denoting the Person's last name.
+     *   @param id - An integer denoting the Person's ID number.
+     *   @param scores - An array of integers denoting the Person's test scores.
+     */
+    // Write your constructor here
 
     Student(String firstName, String lastName, int identification, int[] testScores) {
         super(firstName, lastName, identification);
 
         this.testScores = testScores;
     }
+    /*
+     *   Method Name: calculate
+     *   @return A character denoting the grade.
+     */
+    // Write your method here
 
-    char calculate() {
-        int total = 0;
+    char calculate () {
 
-        for (int testScore : testScores) total += testScore;
+        int total= 0;
 
-        int avg = total / testScores.length;
+        for (int testScore : testScores) total +=testScore;
+
+        int avg = total/testScores.length;
 
         if (avg >= 90 && avg <= 100) return 'O';
         if (avg >= 80 && avg < 90) return 'E';
@@ -40,10 +59,12 @@ class Student extends Person {
         if (avg >= 55 && avg < 70) return 'P';
         if (avg >= 40 && avg < 55) return 'D';
         return 'T';
+
     }
 }
 
-public class Solution {
+
+class Solution {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         String firstName = scan.next();
@@ -51,7 +72,7 @@ public class Solution {
         int id = scan.nextInt();
         int numScores = scan.nextInt();
         int[] testScores = new int[numScores];
-        for (int i = 0; i < numScores; i++) {
+        for(int i = 0; i < numScores; i++){
             testScores[i] = scan.nextInt();
         }
         scan.close();
